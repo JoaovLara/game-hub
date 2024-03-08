1f9125bcef0332a915983feedac0dcae732daa36
Joao Lara <joaovdelara@gmail.com>
2024-03-08 10:53:21 -0300

    Fetch the games

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,17 +1,17 @@
 import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react'
 
 const ColorModeSwitch = () => {
-    const {toggleColorMode, colorMode} = useColorMode();
+    const { toggleColorMode, colorMode } = useColorMode();
 
-  return (
-    <HStack>
-        <Switch isChecked={colorMode === 'dark'} onChange={toggleColorMode}/>
-        <Text>Dark Mode</Text>
-    
+    return (
+        <HStack>
+            <Switch isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
+            <Text>Dark Mode</Text>
 
-    </HStack>
 
-  )
+        </HStack>
+
+    )
 }
 
 export default ColorModeSwitch
---
+++ src/services/api-client.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL:'https://api.rawg.io/api',
+    params: {
+        key: 'cb686a735d2a4addaf975fb6d84a0dda',
+    }
+})
