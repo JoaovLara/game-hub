bdf2b76f96805636abfb9cc8deb9dc60b865f5da
Joao Lara <joaovdelara@gmail.com>
2024-03-11 08:42:03 -0300

    Get optimized images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -1,15 +1,15 @@
 import { Badge } from '@chakra-ui/react';
 
 interface Props {
-    score:number;
+    score: number;
 }
 
 const CriticScore = ({ score }: Props) => {
-    let color = score > 75 ? 'green' : score  > 60 ? 'yellow' : '';
+    let color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
 
-  return (
-    <Badge colorScheme={color} fontSize='14px' paddingX={2}>{score}</Badge>
-  )
-}
+    return (
+        <Badge colorScheme={color} fontSize='14px' paddingX={2}>{score}</Badge>
+    )
+};
 
-export default CriticScore
+export default CriticScore;
