2ca93419777c1ee8f77a121d3569395379dac484
Joao Lara <joaovdelara@gmail.com>
2024-03-11 10:15:34 -0300

    Fetch the genres

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,22 +5,22 @@ import CriticScore from './CriticScore'
 import PlatformIconList from './PlatformIconList'
 
 interface Props {
-  game: Game
+    game: Game
 }
 
 const GameCard = ({ game }: Props) => {
-  return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
-        <HStack justifyContent='space-between'>
-          <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
-  )
+    return (
+        <Card>
+            <Image src={getCroppedImageUrl(game.background_image)} />
+            <CardBody>
+                <Heading fontSize='2xl'>{game.name}</Heading>
+                <HStack justifyContent='space-between'>
+                    <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+                    <CriticScore score={game.metacritic} />
+                </HStack>
+            </CardBody>
+        </Card>
+    )
 }
 
 export default GameCard
