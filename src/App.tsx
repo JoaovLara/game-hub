bc095b509ece7cbf6fc9b09f373887bfe2ec7927
Joao Lara <joaovdelara@gmail.com>
2024-03-14 11:23:52 -0300

    Filter games by genre

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
+import { useState } from "react";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
+import NavBar from "./components/NavBar";
+import { Genre } from "./hooks/useGenres";
 
 function App() {
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+
   return (
     <Grid
       templateAreas={{
@@ -15,20 +19,18 @@ function App() {
         lg: '200px 1fr'
       }}
     >
-
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList />
+          <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} />
         </GridItem>
       </Show>
       <GridItem area="main">
-        <GameGrid />
+        <GameGrid selectedGenre={selectedGenre} />
       </GridItem>
     </Grid>
-
   );
 }
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,17 +1,23 @@
 import useData from "./useData";
-export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
-};
-export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
-};
+import { Genre } from "./useGenres";
 
-const useGames = () => useData<Game>('/games');
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+}
+
+const useGames = (selectedGenre: Genre | null) =>
+  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
+    selectedGenre?.id,
+  ]);
 
 export default useGames;
